bf13ccdbb6448f43aca9fc839cd2d2f506540c52
subijayb <subijay.bhattacharya@nokia.com>
2024-04-18 17:42:07 +0530

    Added test case for StandaloneTestCase that will use cfg file to create hibernate session

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/ORMStandaloneTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/ORMStandaloneTestCase.java
@@ -1,9 +1,13 @@
 package org.hibernate.bugs;
 
+import java.util.List;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,7 +19,7 @@ public class ORMStandaloneTestCase {
 
 	private SessionFactory sf;
 
-	@Before
+	//@Before
 	public void setup() {
 		StandardServiceRegistryBuilder srb = new StandardServiceRegistryBuilder()
 			// Add in any settings that are specific to your test. See resources/hibernate.properties for the defaults.
@@ -30,11 +34,26 @@ public class ORMStandaloneTestCase {
 
 		sf = metadata.buildSessionFactory();
 	}
+	
+	@Before
+	public void setupConfig() {
+		Configuration config = new Configuration();
+		sf = config.configure().buildSessionFactory();
+	}
 
 	// Add your tests, using standard JUnit.
 
 	@Test
 	public void hhh123Test() throws Exception {
+		Session session = sf.openSession();
+		
+		org.hibernate.query.Query<Subscriber> cq = session.createQuery("from Subscriber ", Subscriber.class);
+		List<Subscriber> cqresult = cq.getResultList();
+		System.out.println("result cq======" + cqresult);
+		System.out.println("result cq:0th element======subscriberid=" + cqresult.get(0).getAccountNumber());
+		
+		session.close();
+		sf.close();
 
 	}
 }
